8601e07856efcb51f93f8b5102f1ad9434230d94
makuntz <maira.kuntz@animati.com.br>
2022-05-28 12:31:03 -0300

    backup

---
+++ makuntz-ProjetoIntegradorII---Univesp-master/src/components/Login/index.js
@@ -0,0 +1,46 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useHistory } from "react-router-dom";
+import toast from "react-hot-toast";
+import "./style.css"
+
+
+const Login = ({setUser}) => {
+
+    const history = useHistory()
+
+    const formSchema = yup.object().shape({
+        name: yup.string().required("Name Required"),
+        ra: yup.string().required("RA required"),
+    })
+
+    const {register, handleSubmit, formState: { errors }} = useForm({
+        resolver: yupResolver(formSchema),
+      })
+
+    const onSubmit = (data) => {
+        
+        setUser(data)
+        history.push("/home")
+        toast.success("Login Success")
+        
+    }
+    
+    return(
+        
+        <div className="loginPage">
+            <button className="registerButton" onClick={() => history.push("/register")}>Register</button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <h1>Login</h1>
+                <input placeholder="Name" {...register("name")} required/>
+                    <span>{errors.name?.message}</span> 
+                <input placeholder="RA (registro acadêmico)" type="number" {...register("ra")} required/>
+                    <span>{errors.ra?.message}</span>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
---
+++ makuntz-ProjetoIntegradorII---Univesp-master/src/pages/RegisterPage/index.js
@@ -0,0 +1,13 @@
+import Register from "../../components/Register";
+
+const RegisterPage = ({setUser}) => {
+
+
+    return (
+        <div>
+            <Register setUser={setUser}/>
+        </div>
+    )
+}
+
+export default RegisterPage;
